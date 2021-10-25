a0d159d9c853f74bf9b4c2a50a97e1180b2fa83e
Ariane Morelato <aniemorelanie@gmail.com>
2021-10-24 23:10:44 -0300

    conteudo

---
+++ script.js
@@ -0,0 +1,53 @@
+
+
+function functMenu (){
+const menuMobile = document.querySelector('.btn-mobile')
+const navMobile = document.querySelector('.js-menu-mobile')
+
+function menuAtivo () {
+  navMobile.classList.toggle('ativo')
+}
+
+menuMobile.addEventListener('click', menuAtivo)
+}
+
+functMenu()
+
+
+
+
+
+
+
+var numSlide = 1;
+mostrarSlide(numSlide);
+
+function mudarSlide(ns) {
+  mostrarSlide(numSlide += ns)
+}
+
+function atualSlide(ns) {
+  mostrarSlide(numSlide = ns)
+}
+
+function mostrarSlide(ns) {
+  var i;
+  var slides = document.querySelectorAll(".card-cafe");
+
+  if (ns > slides.length) {
+    numSlide = 1;
+  }
+
+  if (ns < 1) {
+    numSlide = slides.length
+  }
+
+  for (i=0; i<slides.length; i++) {
+    slides[i].style.display="none"
+  }
+
+
+  slides[numSlide-1].style.display = "block";
+}
+
+AOS.init();
