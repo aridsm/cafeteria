e1f66ff3e0f381304cff40f9746cb46fe03366e2
aridsm <aniemorelanie@gmail.com>
2022-12-12 20:26:55 -0300

    added slide products

--- src/components/BestSeller.js
+++ src/components/BestSeller.js
@@ -1,12 +1,49 @@
 import React from "react";
 import "../Styles/Components/BestSeller.scss";
+import { Swiper, SwiperSlide } from "swiper/react";
+import img1 from "../assets/prod1.webp";
+import img2 from "../assets/prod2.webp";
+import img3 from "../assets/prod3.webp";
+import img4 from "../assets/prod4.webp";
+import img5 from "../assets/prod5.webp";
+import "swiper/css/navigation";
+import "swiper/css";
 
 const products = [
   {
-    name: "1",
-    description: "lorem ipsum",
+    name: "lorem ipsum",
+    description: "Feugiat nibh sed pulvinar proin",
     price: "$ 7.99",
     cal: "185kcal",
+    img: img1,
+  },
+  {
+    name: "lorem ipsum",
+    description: "Nibh tellus molestie nunc non",
+    price: "$ 7.99",
+    cal: "185kcal",
+    img: img2,
+  },
+  {
+    name: "lorem ipsum",
+    description: "Id porta nibh venenatis cras sed",
+    price: "$ 7.99",
+    cal: "185kcal",
+    img: img3,
+  },
+  {
+    name: "lorem ipsum",
+    description: "Et pharetra pharetra massa massa ultricies",
+    price: "$ 7.99",
+    cal: "185kcal",
+    img: img4,
+  },
+  {
+    name: "lorem ipsum",
+    description: "Feugiat nibh sed pulvinar proin",
+    price: "$ 7.99",
+    cal: "185kcal",
+    img: img5,
   },
 ];
 
@@ -16,11 +53,23 @@ const BestSeller = () => {
       <div className="container">
         <h2 className="h2">Our best seller</h2>
         <p>Quis hendrerit dolor magna</p>
-        <ul>
+        <Swiper
+          spaceBetween={50}
+          slidesPerView={4}
+          scrollbar={{ draggable: true }}
+        >
           {products.map((prod) => (
-            <li key={prod.name}>{prod.name}</li>
+            <SwiperSlide key={prod.name} className="slide-item">
+              <a href="/">
+                <img src={prod.img} alt={prod.name} />
+                <span className="cal">{prod.cal}</span>
+                <h3>{prod.name}</h3>
+                <p className="paragraph">{prod.description}</p>
+                <span className="price">{prod.price}</span>
+              </a>
+            </SwiperSlide>
           ))}
-        </ul>
+        </Swiper>
       </div>
     </section>
   );
