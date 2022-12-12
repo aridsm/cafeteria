7253a94dca998f43b18f9c6df85d9259d8645853
aridsm <aniemorelanie@gmail.com>
2022-12-12 09:26:17 -0300

    section about added

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,30 @@
 import React from "react";
+import "../Styles/Components/About.scss";
+import img from "../assets/about-img.jpg";
 
 const About = () => {
-  return <section>About</section>;
+  return (
+    <section className="about section-pd">
+      <div className="container">
+        <div className="col-1">
+          <h2 className="h2">About us</h2>
+          <p className="paragraph">
+            Neque egestas congue quisque egestas. Lectus sit amet est placerat
+            in egestas erat. Pellentesque elit eget gravida cum sociis natoque.
+          </p>
+          <p className="paragraph">
+            Erat pellentesque adipiscing commodo elit at imperdiet. Amet est
+            placerat in egestas erat imperdiet sed euismod nisi.
+          </p>
+
+          <a href="/" className="btn">
+            See more
+          </a>
+        </div>
+        <img src={img} alt="coffee" className="img-about" />
+      </div>
+    </section>
+  );
 };
 
 export default About;
